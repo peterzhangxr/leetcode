2917c0e3b5bb63aad4031b74009149b3252fbaf6
Peter Zhang <peter.zhang.henan@gmail.com>
2020-05-23 18:03:56 +0800

    平衡二叉树

--- is-balanced-tree/index.js
+++ is-balanced-tree/index.js
@@ -11,4 +11,54 @@
  */
 var isBalanced = function(root) {
 
+  //方法一 暴力方法
+  //自顶向下递归
+  //问题高度计算冗余太多
+  //nLog(n)
+  /**
+   * @param {TreeNode} root
+   * @return {number}
+  */
+
+  /*
+  var height = function(node) {
+    if (node == null) {
+      return -1
+    }
+
+    return 1 + Math.max(height(node.left), height(node.right))
+  }
+
+  if (root == null) {
+    return true
+  }
+
+  return Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right)
+  */
+
+  //方法二
+  //从底至顶返回子树最大高度，若判定某子树不是平衡树则 “剪枝” ，直接向上返回。
+
+  var isBalanced2 = function(node) {
+    if (node == null) {
+      return 0
+    }
+
+    let left = isBalanced2(node.left)
+    if (left == -1) {
+      return -1
+    }
+
+    let right = isBalanced2(node.right)
+    if (right == -1) {
+      return -1
+    }
+
+    return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1
+  }
+
+  return isBalanced2(root) != -1
+
+
+
 };
